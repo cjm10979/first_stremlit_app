78e29ee9911fafe159d6e9d4629edf83edc2f8a6
cjm10979 <49497448+cjm10979@users.noreply.github.com>
2022-10-29 16:14:11 +0100

    Update streamlit_app.py
    create an input variable called fruit_choice which asks the user to input a fruit of choice
    change the fruit of the api call to the fruit_choice variable

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,10 +22,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 # Bring in requests library
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # put the json data into a pandas dataframe to flatten or normalize it 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
